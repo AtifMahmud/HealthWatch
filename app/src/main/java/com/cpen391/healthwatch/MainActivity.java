7e57ea37742880c978be464022291bff36350720
William Ou <ouhaoqian@hotmail.com>
2018-03-04 18:47:03 -0800

    - Refactored code - Need to modify the map icons such that they appear only at a certain zoom level - Need to add unit tests and possibly change the code to use an abstraction of a map in the process

--- app/src/main/java/com/cpen391/healthwatch/MainActivity.java
+++ app/src/main/java/com/cpen391/healthwatch/MainActivity.java
@@ -1,7 +1,18 @@
+/**
+ * This is the activity to launch the maps activity. The activity is not meant to belong in the main project as a functional activity, rather it is
+ * here to aid testing of the
+ */
+
 package com.cpen391.healthwatch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.cpen391.healthwatch.map.MapsActivity;
+import com.example.atifm.healthwatch.R;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +20,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button testMapsButton = (Button) findViewById(R.id.testMap);
+        testMapsButton.setOnClickListener(new View.OnClickListener(){
+
+            // Navigate to the map activity
+            public void onClick(View v){
+                showMap();
+            }
+        });
+    }
+
+    // Navigate to the map activity
+    public void showMap(){
+        Intent mapIntent = new Intent(this, MapsActivity.class);
+        startActivity(mapIntent);
     }
 }
---
+++ app/src/main/java/com/cpen391/healthwatch/map/CustomIcons.java
@@ -0,0 +1,20 @@
+package com.cpen391.healthwatch.map;
+
+import com.example.atifm.healthwatch.R;
+import com.google.android.gms.maps.model.BitmapDescriptor;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+
+/**
+ * Created by william on 2018/3/4.
+ * A class that is used to obtain icon images for markers.
+ */
+class CustomIcons {
+    static BitmapDescriptor getIcon(String type) {
+        switch(type) {
+            case "ambulance": return BitmapDescriptorFactory.fromResource(R.drawable.ambulance_icon);
+            case "hospital": return BitmapDescriptorFactory.fromResource(R.drawable.hospital_icon);
+            default:
+                return BitmapDescriptorFactory.fromResource(R.drawable.hospital_icon);
+        }
+    }
+}
