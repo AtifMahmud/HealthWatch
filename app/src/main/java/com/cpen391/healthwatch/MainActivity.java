19d23f3a495cedd3251d5fb6d9e7a888ca0ada07
Atif Mahmud <atifmahmud95@gmail.com>
2018-03-05 15:49:55 -0800

    Created activity for patient

--- app/src/main/java/com/cpen391/healthwatch/MainActivity.java
+++ app/src/main/java/com/cpen391/healthwatch/MainActivity.java
@@ -1,7 +1,10 @@
 package com.cpen391.healthwatch;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,19 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button testpageButton = (Button) findViewById(R.id.testPatientPage);
+        testpageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showPatientPage();
+            }
+        });
     }
+
+    private void showPatientPage(){
+        Intent patientPageIntent = new Intent(this, PatientActivity.class);
+        startActivity(patientPageIntent);
+    }
+
 }
