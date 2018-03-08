702e0aebb477afe7b63b56e6b6c9c9c68f2ffd3d
William Ou <ouhaoqian@hotmail.com>
2018-03-08 15:13:28 -0800

    - Added basic sign up page xml

---
+++ app/src/main/java/com/cpen391/healthwatch/user/SignUpActivity.java
@@ -0,0 +1,19 @@
+package com.cpen391.healthwatch.user;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.cpen391.healthwatch.R;
+
+/**
+ * Created by william on 2018/3/8.
+ *
+ */
+public class SignUpActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_signup);
+    }
+}
