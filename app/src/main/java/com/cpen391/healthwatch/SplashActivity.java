cb529f2b666f44610f4bcd503bb7c6de3b555c75
William Ou <ouhaoqian@hotmail.com>
2018-03-07 20:43:36 -0800

    - Added splash screen

--- app/src/main/java/com/cpen391/healthwatch/SplashActivity.java
+++ app/src/main/java/com/cpen391/healthwatch/SplashActivity.java
@@ -2,36 +2,19 @@ package com.cpen391.healthwatch;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 
 import com.cpen391.healthwatch.server.abstraction.AppControlInterface;
-import com.cpen391.healthwatch.server.abstraction.ServerCallback;
 import com.cpen391.healthwatch.server.implementation.AppControl;
 import com.cpen391.healthwatch.server.implementation.ServerContact;
 import com.cpen391.healthwatch.util.GlobalFactory;
 
-public class MainActivity extends AppCompatActivity {
+public class SplashActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
         initClasses();
-        test();
-    }
-
-    /**
-     * Test server connection.
-     */
-    private void test() {
-        GlobalFactory.getServerInterface().asyncGet("/gateway/auth",
-                new ServerCallback() {
-                    @Override
-                    public void onSuccessResponse(String response) {
-                        Log.d("Main", "Obtained: " + response);
-                    }
-                });
     }
 
     /**
