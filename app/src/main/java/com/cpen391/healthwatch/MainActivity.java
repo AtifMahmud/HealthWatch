f735957eb15d795a8bbd290714f948968af4e235
Atif Mahmud <atifmahmud95@gmail.com>
2018-03-18 23:24:46 -0700

    Navigates to new page, need to fix GUI of it and add functions:

--- app/src/main/java/com/cpen391/healthwatch/MainActivity.java
+++ app/src/main/java/com/cpen391/healthwatch/MainActivity.java
@@ -1,7 +1,13 @@
 package com.cpen391.healthwatch;
 
+
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.cpen391.healthwatch.Caretaker.CareTakerActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Button caretakerButton = (Button) findViewById(R.id.test_activity_button);
+        caretakerButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                showCareTakerPage();
+            }
+        });
     }
+
+    public void showCareTakerPage(){
+        Intent careTakerIntent = new Intent(this, CareTakerActivity.class);
+        startActivity(careTakerIntent);
+    }
+
+
 }
