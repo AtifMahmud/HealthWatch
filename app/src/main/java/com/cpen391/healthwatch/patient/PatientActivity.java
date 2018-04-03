a0e072a63410bae7d88509126331eabddcf2178a
Atif Mahmud <atifmahmud95@gmail.com>
2018-04-02 21:33:16 -0700

    making progress

--- app/src/main/java/com/cpen391/healthwatch/patient/PatientActivity.java
+++ app/src/main/java/com/cpen391/healthwatch/patient/PatientActivity.java
@@ -9,6 +9,7 @@ import android.os.Bundle;
 import android.os.IBinder;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
@@ -20,6 +21,7 @@ import android.widget.Toast;
 import com.cpen391.healthwatch.R;
 import com.cpen391.healthwatch.bluetooth.BluetoothService;
 import com.cpen391.healthwatch.bluetooth.BluetoothService.OnBluetoothDataListener;
+import com.cpen391.healthwatch.caretaker.PatientListDividerItemDecoration;
 import com.cpen391.healthwatch.user.UserProfileOperator;
 import com.cpen391.healthwatch.user.UserProfileOperator.UserProfileImageListener;
 import com.cpen391.healthwatch.util.BitmapDecodeTask;
@@ -40,7 +42,7 @@ public class PatientActivity extends AppCompatActivity {
     private UserProfileOperator mImageUploader;
 
     private RecyclerView mRecycllerview;
-    private MealListAdapter mealListAdapter;
+    private MealListAdapter mMealListAdapter;
 
     private ServiceConnection mBluetoothServiceConnection = new ServiceConnection() {
         @Override
@@ -89,7 +91,13 @@ public class PatientActivity extends AppCompatActivity {
     }
 
     private void setupRecyclerView(){
-
+        mRecycllerview = findViewById(R.id.patient_profile_recycler_view);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        mRecycllerview.setLayoutManager(layoutManager);
+        mMealListAdapter = new MealListAdapter(this);
+        mRecycllerview.setAdapter(mMealListAdapter);
+        //RecyclerView.ItemDecoration dividerItemDecoration  = new MealListDividerItemDecoration(getApplicationContext(), R.drawable.inset_divider);
+        //mRecycllerview.addItemDecoration(dividerItemDecoration);
     }
 
     private void setupBluetoothServiceCallbacks() {
