e36a8196db574fa0fdb46ee52b412c2ae1b225ad
Atif Mahmud <atifmahmud95@gmail.com>
2018-04-03 21:32:36 -0700

    Displays meals nicely from JSON

--- app/src/main/java/com/cpen391/healthwatch/patient/PatientProfileAdapter.java
+++ app/src/main/java/com/cpen391/healthwatch/patient/PatientProfileAdapter.java
@@ -16,6 +16,7 @@ import android.widget.TextView;
 import com.cpen391.healthwatch.R;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
@@ -31,19 +32,48 @@ public class PatientProfileAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     static final int TYPE_MEAL_ITEM = 1;
     private Context mContext;
 
-    private List<String> mMealList;
-    private JSONObject meal = new JSONObject();
+    private JSONObject breakfast = new JSONObject();
+    private JSONObject lunch = new JSONObject();
+    private JSONObject dinner = new JSONObject();
     private JSONArray mealList = new JSONArray();
 
     PatientProfileAdapter(Context context) {
-        mMealList = new ArrayList<>();
 
-        // Add dummy JSON
-        for (String item : new String[]{"Pancakes", "Eggs", "Sausages", "Burgers"}) {
-
-            // PArse and add items
-            mMealList.add(item);
+        try {
+            // Add dummy JSON
+            breakfast.put("title", "Breakfast");
+            breakfast.put("time", "9");
+            breakfast.put("item1", "Oatmeal with skim milk and strawberries");
+            breakfast.put("item2", "2 Boiled Eggs");
+        } catch (JSONException e){
+            e.printStackTrace();
         }
+
+
+        try {
+            // Add dummy JSON
+            lunch.put("title", "Lunch");
+            lunch.put("time", "12");
+            lunch.put("item1", "Caesar Salad");
+            lunch.put("item2", "Grilled Chicken");
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+
+
+        try {
+            // Add dummy JSON
+            dinner.put("title", "Dinner");
+            dinner.put("time", "5");
+            dinner.put("item1", "Spaghetti");
+            dinner.put("item2", "250gm Beef Steak");
+        } catch (JSONException e){
+            e.printStackTrace();
+        }
+
+        mealList.put(breakfast);
+        mealList.put(lunch);
+        mealList.put(dinner);
 
         mContext = context;
     }
@@ -65,15 +95,21 @@ public class PatientProfileAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
         if (holder instanceof MealViewHolder) {
             MealViewHolder vh = (MealViewHolder) holder;
-
-            // Add specific field to specific textview
-            vh.mTextView.setText(mMealList.get(position - 1));
+            try {
+                JSONObject meal = mealList.getJSONObject(position - 1);
+                vh.mTitle.setText(meal.getString("title"));
+                vh.mTime.setText(meal.getString("time"));
+                vh.mItem1.setText(meal.getString("item1"));
+                vh.mItem2.setText(meal.getString("item2"));
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     @Override
     public int getItemCount() {
-        return 1 + mMealList.size();
+        return 1 + mealList.length();
     }
 
     @Override
@@ -107,10 +143,17 @@ public class PatientProfileAdapter extends RecyclerView.Adapter<RecyclerView.Vie
     }
 
     class MealViewHolder extends RecyclerView.ViewHolder {
-        TextView mTextView;
+        TextView mTitle;
+        TextView mTime;
+        TextView mItem1;
+        TextView mItem2;
+
         MealViewHolder(View view) {
             super(view);
-            mTextView = view.findViewById(R.id.meal_item);
+            mTitle = view.findViewById(R.id.meal_title);
+            mTime = view.findViewById(R.id.meal_time);
+            mItem1 = view.findViewById(R.id.meal_item1);
+            mItem2 = view.findViewById(R.id.meal_item2);
         }
     }
 }
