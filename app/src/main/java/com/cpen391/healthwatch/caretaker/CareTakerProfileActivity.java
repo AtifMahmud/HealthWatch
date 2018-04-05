863f378790f190ecd17ea0e16d2ebf8381206a84
unknown <mark_c1995@hotmail.com>
2018-04-05 09:54:10 -0700

    Started niew caretaker

--- app/src/main/java/com/cpen391/healthwatch/caretaker/CareTakerProfileActivity.java
+++ app/src/main/java/com/cpen391/healthwatch/caretaker/CareTakerProfileActivity.java
@@ -6,11 +6,24 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.View;
 
+import com.android.volley.VolleyError;
 import com.cpen391.healthwatch.R;
+import com.cpen391.healthwatch.server.abstraction.ServerCallback;
+import com.cpen391.healthwatch.server.abstraction.ServerErrorCallback;
 import com.cpen391.healthwatch.user.UserProfileOperator;
+import com.cpen391.healthwatch.util.AnimationOperator;
 import com.cpen391.healthwatch.util.FadeInNetworkImageView;
+import com.cpen391.healthwatch.util.GlobalFactory;
 import com.cpen391.healthwatch.util.StandardDividerItemDecoration;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by william on 2018-04-04.
@@ -35,6 +48,7 @@ public class CareTakerProfileActivity extends AppCompatActivity {
         setupRecyclerView();
         mImageOperator = new UserProfileOperator();
         mProfileImage = findViewById(R.id.image_cover);
+        getCaretakerProfileFromServer();FromServer();
     }
 
     private void setupRecyclerView() {
@@ -43,8 +57,24 @@ public class CareTakerProfileActivity extends AppCompatActivity {
         mRecyclerView.setLayoutManager(layoutManager);
         mPatientListAdapter = new PatientListAdapter(this);
         mRecyclerView.setAdapter(mPatientListAdapter);
-        mRecyclerView.getRecycledViewPool().setMaxRecycledViews(PatientListAdapter.TYPE_HEADER, 0);
+       // mRecyclerView.getRecycledViewPosetMaxRecycledViews(PatientListAdapter.TYPE_HEADER, 0);
         RecyclerView.ItemDecoration dividerItemDecoration = new StandardDividerItemDecoration(getApplicationContext(), R.drawable.inset_divider);
         mRecyclerView.addItemDecoration(dividerItemDecoration);
+    }
+
+    private void getCaretakerProfileFromServer() {
+        Map<String, String> headers = new HashMap<>();
+        headers.put("token", GlobalFactory.getUserSessionInterface().getUserToken());
+        GlobalFactory.getServerInterface().asyncGet("/gateway/caretaker", headers, new ServerCallback() {
+            @Override
+            public void onSuccessResponse(String response) {
+                Log.d(TAG, "Obtained caretaker profile: " + response);
+            }
+        }, new ServerErrorCallback() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.d(TAG, "Trying to obtain caretaker profile obtained error");
+            }
+        });
     }
 }
